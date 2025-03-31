9f18348ab4502e3ed76e071548ddcb81c6dff01e
QiaoqingWu-AC <wu000302@algonquinlive.com>
2025-03-31 15:18:16 -0400

    create, verify, and deploy backend tf config

--- infra/tf-backend/main.tf
+++ infra/tf-backend/main.tf
@@ -14,7 +14,44 @@ provider "azurerm" {
 }
 
 # Resource Group
-resource "azurerm_resource_group" "lab12_rg" {
-  name     = "wu000302-lab12-rg"
+resource "azurerm_resource_group" "rg" {
+  name     = "wu000302-githubactions-rg"
   location = "canadacentral"
+}
+
+# Storage Account
+resource "azurerm_storage_account" "sa" {
+  name                     = "wu000302githubactions"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
+
+  allow_nested_items_to_be_public = false
+}
+
+# Storage Container
+resource "azurerm_storage_container" "container" {
+  name                  = "tfstate"
+  storage_account_name  = azurerm_storage_account.sa.name
+  container_access_type = "private"
+}
+
+# Outputs
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.sa.name
+}
+
+output "storage_container_name" {
+  value = azurerm_storage_container.container.name
+}
+
+output "storage_account_access_key" {
+  value     = azurerm_storage_account.sa.primary_access_key
+  sensitive = true
 }
