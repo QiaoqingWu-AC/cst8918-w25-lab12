7deff95f10fed4405400b3157ea16fc01d06f356
QiaoqingWu-AC <wu000302@algonquinlive.com>
2025-03-31 17:05:28 -0400

    Create Azure credentials for GitHub Actions

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -1,4 +1,10 @@
+# Resource Group
 resource "azurerm_resource_group" "a12_rg" {
   name     = "wu000302-a12-rg"
   location = "canadacentral"
+}
+
+# Output
+output "resource_group_name" {
+  value = azurerm_resource_group.a12_rg.name
 }
