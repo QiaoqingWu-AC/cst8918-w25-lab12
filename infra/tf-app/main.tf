a32cddaae431aa10784b26c5a947b058c3925187
Yue0218 <gaoyue.218@gmail.com>
2025-03-31 19:47:17 -0400

    add description in `resource_group_name` output

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -7,4 +7,5 @@ resource "azurerm_resource_group" "a12_rg" {
 # Output
 output "resource_group_name" {
   value = azurerm_resource_group.a12_rg.name
+  description = "The name of the Azure Resource Group"
 }
