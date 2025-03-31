6af69d97b3f2b2c77c988463caecf77a43508366
QiaoqingWu-AC <wu000302@algonquinlive.com>
2025-03-31 14:40:29 -0400

    initial main.tf and test the rule

---
+++ infra/tf-backend/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 1.3.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+# Resource Group
+resource "azurerm_resource_group" "lab12_rg" {
+  name     = "wu000302-lab12-rg"
+  location = "canadacentral"
+}
