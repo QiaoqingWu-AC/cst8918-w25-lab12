f9d88b774e1b89ed028573ebe219f9fd85923c99
Yue0218 <gaoyue.218@gmail.com>
2025-03-31 19:52:28 -0400

    Fix terraform formatting

--- infra/tf-app/main.tf
+++ infra/tf-app/main.tf
@@ -6,6 +6,6 @@ resource "azurerm_resource_group" "a12_rg" {
 
 # Output
 output "resource_group_name" {
-  value = azurerm_resource_group.a12_rg.name
+  value       = azurerm_resource_group.a12_rg.name
   description = "The name of the Azure Resource Group"
 }
